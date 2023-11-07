a379268e430e29763d011a855ab7e9f2cdcefc4f
macalderonq <ma.calderonq@duocuc.cl>
2023-11-06 22:03:31 -0300

    prueba de conflicto 1

--- prueba.java
+++ prueba.java
@@ -11,7 +11,7 @@ public class prueba {
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
         
-        System.out.println("Probando...");
+        System.out.println("Texto de la cote");
     }
    
 }
